28c98f6f534378cf9b79c1a5e0e61e241409c7fd
campos-joao <jvictor.contato@gmail.com>
2023-02-13 18:05:24 -0300

    projeto alura space

--- galeria/views.py
+++ galeria/views.py
@@ -1,19 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
 
-dados = {
-     1 : { 'nome' : 'Nebulosa de Carina',
-     'legenda' : 'webbtelecop.org / NASA / James Webb' },
-     2 : { 'nome' : 'Galaxia NGC 1079',
-     'legenda' : 'nasa.org / NASA / Hubble' }
-
-}
+from galeria.models import Fotografia
 
 def index(request):
-    
-    return render(request, 'galeria/index.html', {'cards' : dados})
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {'cards' : fotografias})
 
-def imagem(request):
+def imagem(request, foto_id):
 
-    return render(request, 'galeria/imagem.html')
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html',{'fotografia': fotografia})
